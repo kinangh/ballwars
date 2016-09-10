6d4d6bed5c9e27ec2a1fedfbbd9162bc8a5c36a9
kinanghnim <kinanghnim@gmail.com>
2016-09-10 21:31:35 +0200

    commit 1
    moving

---
+++ Assets/scripts/movment.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movment : MonoBehaviour {
+
+    public float speed;
+
+    private bool onground;
+    public float jumppressure;
+
+	void Start()
+    {
+        onground = true;   
+    }
+
+	void Update () {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        Vector2 movment = new Vector3(movehorizontal, 0, 0);
+        GetComponent<Rigidbody2D>().AddForce(movment * Time.deltaTime * speed);
+
+        if (Input.GetButton("Jump"))
+        {
+            if (onground)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0f, jumppressure);
+                onground = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            onground = true;
+        }
+    }
+}
